fb575131f2f58eb5fe9e2f57cb5eb5efbb35148d
Feroz Ally <feroz.ally@derivco.co.za>
2016-11-01 14:55:17 +0200

    Added People.cs

---
+++ Test Console App/People.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Console_App
+{
+    class People
+    {
+        public string Name { get; set; }
+        public int IDNo { get; set; }
+    }
+}
